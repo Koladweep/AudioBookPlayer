58f2d972f8412073d9074494dc603c6829840af1
koladweep <102764937+Koladweep@users.noreply.github.com>
2025-02-27 19:01:14 +0530

    translated user faces text and docstrings to English

--- ABPlayer/drivers/downloadi_process_handlers/stdout_handler.py
+++ ABPlayer/drivers/downloadi_process_handlers/stdout_handler.py
@@ -5,8 +5,8 @@ from ..base import BaseDownloadProcessHandler
 
 class StdoutDownloadProcessHandler(BaseDownloadProcessHandler):
     """
-    Обработчик процесса скачивания.
-    Визуализирует процесс скачивания книги в консоли.
+    Download process handler.
+    Visualizes the book download process in the console.
     """
 
     def show_progress(self) -> None:
@@ -15,3 +15,4 @@ class StdoutDownloadProcessHandler(BaseDownloadProcessHandler):
             f"{round(self.done_size / (self.total_size / 100), 2)} %"
         )
         sys.stdout.flush()
+
